6d429afa5c0070736fc4442d72ac345263469415
Forrest York <forrest@eyesopen.com>
2020-11-21 21:15:09 -0700

    Swap model to be oriented towards regression
    * Load data using a custom means, which correctly assigns
      floating point numbers to the images that correspond to pull
      time.
    * Switch to using MSE error
    * Use a linear activation function at the end of the model

--- optpresso/training.py
+++ optpresso/training.py
@@ -8,56 +8,88 @@ import os
 from typing import List, Any
 from argparse import Namespace, ArgumentParser
 
+import numpy as np
+
+from keras.optimizers import Adam
 from keras.utils import plot_model
 from keras.models import Sequential
 from keras.layers import Activation, Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import img_to_array, load_img
+
+IMG_EXTS = [".jpg", ".png"]
 
 
 def train(parent_args: Namespace, leftover: List[str]):
     parser = ArgumentParser()
     parser.add_argument("directory")
     parser.add_argument("--batch_size", default=5, type=int)
+    parser.add_argument("--epochs", default=200, type=int)
     args = parser.parse_args(leftover)
 
-    train_datagen = ImageDataGenerator(
-        rescale=1.0 / 255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True,
-        validation_split=0.1,
-        rotation_range=90,
-    )
+    generator = GroundsLoader(args.directory, args.batch_size, (255, 255))
 
-    # Lots of work around if this is classifying things correctly or if i
-    # need to roll my own data loader
-    labels = [int(x) for x in sorted(os.listdir(args.directory)) if os.path.isdir(x)]
-
-    generator = train_datagen.flow_from_directory(
-        args.directory,
-        target_size=(255, 255),
-        class_mode="binary",
-        classes=labels,
-        batch_size=args.batch_size
-    )
     model = build_model((255, 255, 3))
 
     model.summary()
-    plot_model(model, to_file="junk-model.png")
 
     model.fit(
-        generator,
-        epochs=200,
+        generator.generator(),
+        epochs=args.epochs,
+        steps_per_epoch=len(generator) // args.batch_size,
         shuffle=True,
         batch_size=args.batch_size,
     )
     model.save("junk-model.h5")
+    plot_model(model, to_file="junk-model.png")
+
+
+class GroundsLoader:
+    """Generator that provides lots of images of ground coffee with
+    data usable for regression, no nasty classification
+    """
+
+    __slots__ = ("_directory", "_batch_size", "_paths", "_target_size")
+
+    def __init__(self, directory: str, batch_size: int, target_size: tuple):
+        self._directory = directory
+        self._batch_size = batch_size
+        self._paths = []
+        self._target_size = target_size
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if os.path.splitext(file)[1].lower() not in IMG_EXTS:
+                    continue
+                self._paths.append(os.path.join(root, file))
+
+    def __len__(self):
+        return len(self._paths)
+
+    def generator(self):
+        # I kind of get it, but still hate the infinite generator
+        total_size = len(self._paths)
+        while True:
+            batch_start = 0
+            batch_end = self._batch_size
+            while batch_start < total_size:
+                limit = min(batch_end, total_size)
+                yield self.get_batch(batch_start, limit)
+                batch_start += self._batch_size
+                batch_end += self._batch_size
+
+    def get_batch(self, start: int, end: int):
+        files = self._paths[start:end]
+        x = np.zeros((len(files), self._target_size[0], self._target_size[0], 3))
+        y = np.zeros((len(files),))
+        for i, path in enumerate(files):
+            x[i] = img_to_array(load_img(path, target_size=self._target_size))
+            y[i] = float(os.path.basename(os.path.dirname(path)))
+        return x, y
 
 
 def build_model(shape: List[int], alpha: float = 0.3) -> List[Any]:
     model = Sequential()
     model.add(Conv2D(32, (3, 3), input_shape=shape))
-    model.add(Activation('relu'))
+    model.add(Activation("relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Conv2D(32, (3, 3)))
@@ -73,9 +105,8 @@ def build_model(shape: List[int], alpha: float = 0.3) -> List[Any]:
     model.add(Activation("relu"))
     model.add(Dropout(0.5))
     model.add(Dense(1))
-    model.add(Activation("sigmoid"))
+    model.add(Activation("linear"))
     # From regression example
-    # opt = keras.optimizers.Adam(lr=1e-4)
-    # model.compile(optimizer=opt, loss="mse", metrics=["mse"])
-    model.compile(loss="binary_crossentropy", optimizer="rmsprop", metrics=["accuracy"])
+    opt = Adam(lr=1e-4)
+    model.compile(optimizer=opt, loss="mse", metrics=["mse"])
     return model
