3b22f8000f3dfe0ec7592dc727ad62247874c2bb
Forrest York <forrest.york@gmail.com>
2020-12-06 15:51:21 -0700

    Improve the eval graph
    * Add MSE, R2
    * Add Polyfit1d best fit

--- optpresso/models/eval.py
+++ optpresso/models/eval.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
+from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.gaussian_process import GaussianProcessRegressor
 
 from optpresso.utils import GroundsLoader
@@ -45,20 +46,23 @@ def evalulate_model(parent_args: Namespace, leftover: List[str]):
         # Give us a clever best fit
         # https://jakevdp.github.io/PythonDataScienceHandbook/04.03-errorbars.html
         y_predict = model.predict(x_test)
-
+        plt.plot(y_test, y_predict, "ro", label="Prediction values", alpha=0.3)
         gp = GaussianProcessRegressor()
         gp.fit(y_test[:, np.newaxis], y_predict)
-        xfit = np.linspace(0, max(y_predict.max(), y_test.max()), 1000)
+        # Start at 3 as that is the floor for the model based on the machine
+        xfit = np.linspace(3, max(y_predict.max(), y_test.max()), 120)
         yfit, mse_err = gp.predict(xfit[:, np.newaxis], return_std=True)
         yfit = yfit.squeeze()
         dyfit = 2 * np.sqrt(mse_err)  # 2*sigma ~ 95% confidence region
         plt.plot(xfit, yfit, "-", color="gray", label="GP Fit")
         plt.fill_between(xfit, yfit - dyfit, yfit + dyfit, color="gray", alpha=0.2)
-
-        plt.plot(y_test, y_predict, "ro", label="Prediction values")
         plt.axline([0, 0], [1, 1])
-        plt.ylabel("Prediction")
-        plt.xlabel("Actual")
+        plt.plot(np.unique(y_test), np.poly1d(np.polyfit(y_test, y_predict.squeeze(), 1))(np.unique(y_test)), color="black", label="Poly1D fit")
+        plt.annotate("r-squared = {:.3f}".format(r2_score(y_test, y_predict)), (0, 1))
+        plt.annotate("mse = {:.3f}".format(mean_squared_error(y_test, y_predict)), (0, 3))
+        plt.ylabel("Predicted Pull time (s)")
+        plt.xlabel("Actual Pull time (s)")
+        plt.legend()
         plt.suptitle(f"Model: {model_path}")
         if not args.write:
             plt.show()
