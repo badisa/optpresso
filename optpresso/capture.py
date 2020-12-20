765fa2197fe0b84cd398bbfb2bcc747de5d0fcb6
Forrest York <forrest.york@gmail.com>
2020-12-19 22:19:46 -0700

    Flip images and mirror them when capturing
    * Provides more data to train against and orientation should not
      be a factor for the model.

--- optpresso/capture.py
+++ optpresso/capture.py
@@ -5,7 +5,28 @@ from typing import List, Any
 from argparse import Namespace, ArgumentParser
 
 import cv2
+from PIL import Image
+from keras.preprocessing.image import load_img
 
+
+def flip_image(path: str, flip_img: bool = True, mirror_img : bool = False):
+    """Flip the image and save files in the same place with the -flip-<int> as a suffix
+
+    If the int is 0 then it was a horizontal flip
+    If the int is 1 then it was a vertical flip
+    if the int is 2 then it was both
+    """
+    name, ext = os.path.splitext(path)
+    img = load_img(path)
+    if mirror_img:
+        img = img.transpose(Image.FLIP_LEFT_RIGHT)
+        img.save(f"{name}-flip-0{ext}")
+    if flip_img:
+        img = img.transpose(Image.FLIP_TOP_BOTTOM)
+        img.save(f"{name}-flip-1{ext}")
+    if flip_img and mirror_img:
+        img = img.transpose(Image.FLIP_TOP_BOTTOM)
+        img.save(f"{name}-flip-2{ext}")
 
 def get_user_input(prompt: str, converter) -> Any:
     while True:
@@ -20,6 +41,7 @@ def capture(parent_args: Namespace, leftover: List[str]):
     parser = ArgumentParser()
     parser.add_argument("camera", default=0, type=int, help="Camera index to use")
     parser.add_argument("--output-dir", default=".", type=str)
+    parser.add_argument("--no-flip", action="store_true", help="Disable flipping and mirroring images")
     parser.add_argument(
         "--machine", default="linea_mini", type=str, help="Espresso machine used"
     )
@@ -36,6 +58,7 @@ def capture(parent_args: Namespace, leftover: List[str]):
     print("c/enter - capture image and prompt for pull time")
     print("q/esc - quit")
     print("------------------")
+    flip_images = not args.no_flip
     try:
         while True:
             ret, frame = cam.read()
@@ -60,7 +83,9 @@ def capture(parent_args: Namespace, leftover: List[str]):
                     os.mkdir(dir_name)
                 output_path = os.path.join(dir_name, f"{now}-{args.machine}.png")
                 cv2.imwrite(output_path, frame)
-                print(f"Wrote frame to {output_path}")
+                if flip_images:
+                    flip_image(output_path, flip_img=True, mirror_img=True)
+                print(f"Wrote frame to {output_path}, Flipped: {flip_images}")
             elif k == 113 or k == 27:  # Hit q or esc to quit
                 print("Quitting")
                 break
