f149af21d511b303d30b6bdac828ab35988a988d
Forrest York <forrest.york@gmail.com>
2020-12-22 12:18:04 -0700

    Enables evaluation during training and using weighted values
    * Turns training into a function, its a hack though as it does way too
      much at this point.
    * Use hard coded weighted values with --weighted flag. No obvious
      improvement so far, still experimenting

--- optpresso/models/training.py
+++ optpresso/models/training.py
@@ -1,18 +1,75 @@
 import os
 import sys
 import math
-from typing import List, Any
+from typing import List, Any, Optional
 from argparse import Namespace, ArgumentParser
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-from keras.utils import plot_model
-from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+from keras.models import load_model
+from keras.callbacks import ModelCheckpoint, EarlyStopping, LearningRateScheduler
 
 from optpresso.utils import GroundsLoader
 from optpresso.data.partition import find_test_paths, k_fold_partition
 from optpresso.models.networks import MODEL_CONSTRUCTORS
+from optpresso.models.eval import graph_model
+from optpresso.data.config import load_config
+
+class PolynomialDecay:
+
+    def __init__(self, num_epochs: int = 100, learning_rate: float = 1e-4, power: float = 1.0):
+        self.num_epochs = num_epochs
+        self.learning_rate = learning_rate
+        self.power = power
+
+    def __call__(self, epoch: int) -> float:
+        decay = (1 - (epoch / float(self.num_epochs))) ** self.power
+        return float(self.learning_rate * decay)
+
+def train_model(args, model, training, validation, callbacks: Optional[List[Any]] = None, fold: Optional[int] = None):
+    if args.weighted:
+        training_gen = training.weighted_training_gen()
+        validation_batch = validation.get_weighted_batch(0, len(validation))
+    else:
+        training_gen = training.training_gen()
+        validation_batch = validation.get_batch(0, len(validation))
+    fit_hist = model.fit(
+        training_gen,
+        epochs=args.epochs,
+        steps_per_epoch=int(math.ceil(len(training) / args.batch_size)),
+        batch_size=args.batch_size,
+        callbacks=callbacks,
+        validation_data=validation_batch,
+    )
+    output_path = args.output_path
+    name, ext = os.path.splitext(output_path)
+    if fold is not None:
+        output_path = f"{name}-fold-{fold}{ext}"
+    model.save(output_path)
+    x = np.linspace(
+        0, len(fit_hist.history["val_loss"]), len(fit_hist.history["val_loss"])
+    )
+    val_label = "Validation Loss"
+    train_label = "Training Loss"
+    if fold is not None:
+        val_label += f" Fold {fold}"
+        train_label += f" Fold {fold}"
+    plt.plot(x, fit_hist.history["val_loss"], label=val_label)
+    plt.plot(x, fit_hist.history["loss"], label=train_label)
+    plt.yscale("log")
+    plt.legend()
+    if args.write:
+        if fold is None:
+            plt.savefig(f"{name}_loss_graph.png")
+        else:
+            plt.savefig(f"{name}_loss_graph_fold_{fold}.png")
+    else:
+        plt.show()
+    plt.close()
+    if args.eval:
+        graph_model(output_path, model, validation, write=args.write)
+    return fit_hist
 
 
 def train(parent_args: Namespace, leftover: List[str]):
@@ -24,6 +81,8 @@ def train(parent_args: Namespace, leftover: List[str]):
     parser.add_argument("--epochs", default=200, type=int)
     parser.add_argument("--height", default=240, type=int)
     parser.add_argument("--width", default=320, type=int)
+    parser.add_argument("--weighted", action="store_true", help="Use sample weights to aim for better fit for tail ends of data")
+    parser.add_argument("--eval", action="store_true", help="Generate evaluation graphs for the validation data")
     parser.add_argument(
         "--write", action="store_true", help="Write out loss graph to loss_graph.png"
     )
@@ -49,6 +108,11 @@ def train(parent_args: Namespace, leftover: List[str]):
         # ReduceLROnPlateau(monitor="val_loss", factor=0.2, patience=10, min_lr=1e-6),
     ]
 
+    config = load_config()
+    comp_model = None
+    if config is not None:
+        comp_model = load_model(config.model)
+
     if args.k_folds is None:    
         generator = GroundsLoader(
             args.batch_size, (args.height, args.width), directory=args.directory,
@@ -62,32 +126,10 @@ def train(parent_args: Namespace, leftover: List[str]):
             validation = GroundsLoader(
                 args.batch_size, (args.height, args.width), directory=args.validation_directory
             )
-            validation = validation.get_batch(0, len(validation))
-
         model = MODEL_CONSTRUCTORS[args.model_name]((args.height, args.width, 3))
-
-        model.summary()
-
-        fit_hist = model.fit(
-            generator.training_gen(),
-            epochs=args.epochs,
-            steps_per_epoch=int(math.ceil(len(generator) / args.batch_size)),
-            batch_size=args.batch_size,
-            callbacks=callbacks,
-            validation_data=validation,
-        )
-        model.save(args.output_path)
-        x = np.linspace(
-            0, len(fit_hist.history["val_loss"]), len(fit_hist.history["val_loss"])
-        )
-        plt.plot(x, fit_hist.history["val_loss"], label="Validation Loss")
-        plt.plot(x, fit_hist.history["loss"], label="Training Loss")
-        plt.yscale("log")
-        plt.legend()
-        if args.write:
-            plt.savefig(f"{args.model_name}_loss_graph.png")
-        else:
-            plt.show()
+        train_model(args, model, generator, validation, callbacks=callbacks)
+        if comp_model is not None and args.eval and validation is not None:
+            graph_model("comparison", comp_model, validation, write=args.write)
     else:
         folds_dir = k_fold_partition(args.directory, folds=args.k_folds)
         fold_to_path = {}
@@ -112,26 +154,10 @@ def train(parent_args: Namespace, leftover: List[str]):
             validation = GroundsLoader(
                 args.batch_size, (args.height, args.width), paths=validation_paths,
             )
-            validation = validation.get_batch(0, len(validation))
 
             model = MODEL_CONSTRUCTORS[args.model_name]((args.height, args.width, 3))
-
-            fit_hist = model.fit(
-                generator.training_gen(),
-                epochs=args.epochs,
-                steps_per_epoch=int(math.ceil(len(generator) / args.batch_size)),
-                batch_size=args.batch_size,
-                callbacks=callbacks,
-                validation_data=validation,
-            )
+            fit_hist = train_model(args, model, generator, validation, callbacks=callbacks, fold=i)
             fold_min.append(min(fit_hist.history["val_loss"]))
-            model.save(f"fold-{i}-{args.output_path}")
-            x = np.linspace(
-                0, len(fit_hist.history["val_loss"]), len(fit_hist.history["val_loss"])
-            )
-            plt.plot(x, fit_hist.history["val_loss"], label=f"Validation Loss: Fold {i}")
-            plt.plot(x, fit_hist.history["loss"], label=f"Training Loss: Fold {i}")
-        plt.yscale("log")
-        plt.legend()
-        plt.savefig(f"{args.model_name}_loss_graph-with-folds.png")
+            if comp_model is not None and args.eval:
+                graph_model(f"comparison-fold-{i}", comp_model, validation, write=args.write)
         print("Average Validation Loss: {}, All: {}".format(np.array(fold_min).mean(), fold_min))
--- optpresso/utils.py
+++ optpresso/utils.py
@@ -26,36 +26,67 @@ class GroundsLoader:
         if directory is None and paths is None:
             raise RuntimeError("Must provide directory or paths")
         if directory is not None:
-            for _, path in find_test_paths(directory):
-                self._paths.append(path)
+            for time, path in find_test_paths(directory):
+                self._paths.append((time, path))
         if paths is not None:
-            self._paths.extend(paths)
+            for path in paths:
+                try:
+                    time = float(os.path.basename(os.path.dirname(path)))
+                except Exception:
+                    print("Skipping path", path)
+                    continue
+                self._paths.append((time, path))
 
     def __len__(self):
         return len(self._paths)
 
     def training_gen(self):
         while True:
+            # I kind of get it, but still hate the infinite generator
             for batch in self.generator():
                 yield batch
 
-    def generator(self):
-        # I kind of get it, but still hate the infinite generator
+    def weighted_training_gen(self):
+        while True:
+            for batch in self.weighted_generator():
+                yield batch
+
+    def _base_gen(self, meth):
         total_size = len(self._paths)
         shuffle(self._paths)
         batch_start = 0
         batch_end = self._batch_size
         while batch_start < total_size:
             limit = min(batch_end, total_size)
-            yield self.get_batch(batch_start, limit)
+            yield meth(batch_start, limit)
             batch_start += self._batch_size
             batch_end += self._batch_size
+
+    def generator(self):
+        return self._base_gen(self.get_batch)
+
+    def weighted_generator(self):
+        return self._base_gen(self.get_weighted_batch)
 
     def get_batch(self, start: int, end: int):
         files = self._paths[start:end]
         x = np.zeros((len(files), self._target_size[0], self._target_size[1], 3))
         y = np.zeros((len(files),))
-        for i, path in enumerate(files):
+        i = 0
+        for time, path in files:
             x[i] = img_to_array(load_img(path, target_size=self._target_size))
-            y[i] = float(os.path.basename(os.path.dirname(path)))
+            y[i] = time
+            i += 1
         return x, y
+
+    def get_weighted_batch(self, start: int, end: int):
+        x, y = self.get_batch(start, end)
+        weights = np.ones(y.shape)
+        for i, time in enumerate(y):
+            if time <= 15.0:
+                weights[i] = 1.5
+            elif time <= 25.0 or time >= 45.0:
+                weights[i] = 1.25
+            elif time >= 28 and time <= 36:
+                weights[i] = 0.8
+        return x, y, weights
