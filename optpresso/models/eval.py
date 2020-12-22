8e895278b22efb84b94770f1555965c7ac12a339
Forrest York <forrest.york@gmail.com>
2020-12-22 12:07:19 -0700

    Make eval graphing a function
    * Want to use it as part of training, to easily see how the validation
      data performs

--- optpresso/models/eval.py
+++ optpresso/models/eval.py
@@ -15,6 +15,45 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from optpresso.utils import GroundsLoader
 from optpresso.data.config import load_config
 
+def graph_model(model_path, model, loader, write: bool = False):
+    i = 0
+    y_predict = []
+    y_test = []
+    for x, y in loader.generator():
+        y_pred = model.predict(x)
+        y_predict.extend(list(y_pred))
+        y_test.extend(list(y))
+    y_predict = np.array(y_predict)
+    y_test = np.array(y_test)
+    plt.plot(y_test, y_predict, "ro", label="Prediction values", alpha=0.3)
+    plt.axline([0, 0], [1, 1])
+    plt.plot(
+        np.unique(y_test),
+        np.poly1d(np.polyfit(y_test, y_predict.squeeze(), 1))(np.unique(y_test)),
+        color="black",
+        label="Poly1D fit",
+    )
+    plt.annotate(
+        "r^2 = {:.3f}".format(r2_score(y_test, y_predict)),
+        (0.7, 0.04),
+        xycoords="axes fraction",
+    )
+    plt.annotate(
+        "mse = {:.3f}".format(mean_squared_error(y_test, y_predict)),
+        (0.7, 0.01),
+        xycoords="axes fraction",
+    )
+    plt.ylabel("Predicted Pull time (s)")
+    plt.xlabel("Actual Pull time (s)")
+    plt.legend()
+    plt.suptitle(f"Model: {model_path}")
+    if not write:
+        plt.show()
+    else:
+        model_name = os.path.basename(model_path).split(".")[0]
+        plt.savefig(f"eval-{model_name}.png")
+    plt.close()
+
 
 def evalulate_model(parent_args: Namespace, leftover: List[str]):
     parser = ArgumentParser()
@@ -41,51 +80,4 @@ def evalulate_model(parent_args: Namespace, leftover: List[str]):
             (model.input_shape[1], model.input_shape[2]),
             directory=args.directory,
         )
-        i = 0
-        y_predict = []
-        y_test = []
-        for x, y in loader.generator():
-            y_pred = model.predict(x)
-            y_predict.extend(list(y_pred))
-            y_test.extend(list(y))
-        y_predict = np.array(y_predict)
-        y_test = np.array(y_test)
-        plt.plot(y_test, y_predict, "ro", label="Prediction values", alpha=0.3)
-        # Give us a clever best fit
-        # https://jakevdp.github.io/PythonDataScienceHandbook/04.03-errorbars.html
-        gp = GaussianProcessRegressor()
-        gp.fit(y_test[:, np.newaxis], y_predict)
-        # Start at 3 as that is the floor for the model based on the machine
-        xfit = np.linspace(3, max(y_predict.max(), y_test.max()), 120)
-        yfit, mse_err = gp.predict(xfit[:, np.newaxis], return_std=True)
-        yfit = yfit.squeeze()
-        dyfit = 2 * np.sqrt(mse_err)  # 2*sigma ~ 95% confidence region
-        plt.plot(xfit, yfit, "-", color="gray", label="GP Fit")
-        plt.fill_between(xfit, yfit - dyfit, yfit + dyfit, color="gray", alpha=0.2)
-        plt.axline([0, 0], [1, 1])
-        plt.plot(
-            np.unique(y_test),
-            np.poly1d(np.polyfit(y_test, y_predict.squeeze(), 1))(np.unique(y_test)),
-            color="black",
-            label="Poly1D fit",
-        )
-        plt.annotate(
-            "r^2 = {:.3f}".format(r2_score(y_test, y_predict)),
-            (0.7, 0.04),
-            xycoords="axes fraction",
-        )
-        plt.annotate(
-            "mse = {:.3f}".format(mean_squared_error(y_test, y_predict)),
-            (0.7, 0.01),
-            xycoords="axes fraction",
-        )
-        plt.ylabel("Predicted Pull time (s)")
-        plt.xlabel("Actual Pull time (s)")
-        plt.legend()
-        plt.suptitle(f"Model: {model_path}")
-        if not args.write:
-            plt.show()
-        else:
-            model_name = os.path.basename(model_path).split(".")[0]
-            plt.savefig(f"eval-{model_name}.png")
-        plt.close()
+        graph_model(model_path, model, loader, write=args.write)
